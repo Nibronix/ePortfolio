954bdc2edcf1a607906e6912a230ef97d250e1f3
Nibronix <nicholas.marolla@gmail.com>
2025-03-03 12:43:06 -0800

    Added cat

--- lambda-nodejs22.x/ePortfolio/src/App.jsx
+++ lambda-nodejs22.x/ePortfolio/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './App.css'
 import Footer from './footer'
+import Crunch from './assets/Crunch.jpg'
 
 function App() {
   const [showGradient, setShowGradient] = useState(false);
@@ -19,6 +20,13 @@ function App() {
       <div className="content">
         <h1 className="fade-in">Hello!</h1>
         <p className="fade-in delay">I'm Nicholas Marolla, a student at CSU Monterey Bay.</p>
+        <p className="fade-in delay-4s" style = {{ marginTop: '5rem'}}>Cat tax.</p>
+        <img
+          src = {Crunch}
+          alt = "My Latina Princess"
+          className = "fade-in delay-4s"
+          style = {{ width: '30%', height: 'auto', borderRadius: '10px'}}
+        />
       </div>
       <Footer />
     </>
