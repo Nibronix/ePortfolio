69ce5e5ce26bc8802edba7cf8e9ddd0250ce2a23
Nibronix <nicholas.marolla@gmail.com>
2025-03-06 11:36:21 -0800

    Finished HW1 main page

--- ePortfolio/src/Hw1.jsx
+++ ePortfolio/src/Hw1.jsx
@@ -14,19 +14,58 @@ function Hw1() {
         return () => clearTimeout(timer);
     }, []);
 
+    const HistoryButton = () => {
+        window.location.href = '/hw1-1';
+    };
+
+    const TodayButton = () => {
+        window.location.href = '/hw1-2';
+    };
+
+    const FutureButton = () => {
+        window.location.href = '/hw1-3';
+    };
+
     return (
         <>
             {showGradient && <div className="gradient-background-hw1" />}
             <Nav />
             <div className="content">
-                <h1 className="fade-in" style={{marginTop: "-10rem"}}>Homework 1 <br/>Virtual Reality</h1>
+                <h1 className="fade-in" style={{marginTop: "10rem"}}>Homework 1 <br/>Virtual Reality</h1>
 
                 <p className="fade-in delay-1s" style={{ fontSize: '24px' }}>Virtual Reality (VR) is a technology that emphasizes on immersing users
                     in digital environments, fundamentally changing the way users interact with content. While VR has been around for years, the last 
                     decade has shown a resurgence in the technology.
                 </p>
 
-                
+                <div className="VR-button-container">
+
+                    <button
+                        onClick={HistoryButton}
+                        className="fade-in delay-2s"
+                        style={{
+                            marginLeft: '0rem'
+                        }}>
+                            HISTORY
+                    </button>
+
+                    <button
+                        onClick={TodayButton}
+                        className="fade-in delay-2s"
+                        style={{
+                            marginCenter: 'auto'
+                        }}>
+                            TODAY'S WORLD
+                    </button>
+
+                    <button
+                        onClick={FutureButton}
+                        className="fade-in delay-2s"
+                        >
+                            THE FUTURE
+                    </button>
+
+                </div>
 
                 <div className="contact-container fade-in delay">
                     
