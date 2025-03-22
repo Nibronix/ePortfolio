fd4d6e8ca6383de8dc0b03890bd238fdbec80b48
Nibronix <nicholas.marolla@gmail.com>
2025-03-22 13:27:58 -0700

    Randomized gender (only m/f) for Fake Person

--- ePortfolio/src/Hw3.jsx
+++ ePortfolio/src/Hw3.jsx
@@ -21,35 +21,44 @@ function Hw3() {
     const [address, setAddress] = useState('');
     const [city, setCity] = useState('');
     const [country, setCountry] = useState('');
+    const [showPerson, setShowPerson] = useState('');
+    const [sex, setSex] = useState('');
     
-    const handlePerson = (e) => {
-        const gender = (Math.random() * 1)
+    const handlePerson = () => {
+        const gender = Math.round(Math.random());
 
-        if (gender) {
-            fetch(`https://fakerapi.it/api/v2/persons?_quantity=1&_gender=male&_birthday_start=2005-01-01`)
-            .then(response => {
-                if(!response.ok) {
-                    throw new Error('Error fetching response.');
-                }
-                return response.json();
-            })
-            .then(data => {
-                if (data === false) {
-                    throw new Error('Error fetching data.');
-                }
-
-                setFirstName(data.data[0].firstname);
-                setLastName(data.data[0].lastname);
-                setEmail(data.data[0].email);
-                setBirthday(data.data[0].birthday);
-                setGender(data.data[0].gender);
-                setAddress(data.data[0].address.street);
-                setCity(data.data[0].address.city);
-                setCountry(data.data[0].address.country);
-
-                console.log(data);
-            });
+        if (gender === 0) {
+            setSex("male");
+        } else {
+            setSex("female");
         }
+
+        console.log("Sex: " + sex);
+        console.log("Gender: " + gender);
+
+        fetch(`https://fakerapi.it/api/v2/persons?_quantity=1&_gender=${sex}&_birthday_start=2005-01-01`)
+        .then(response => {
+            if(!response.ok) {
+                throw new Error('Error fetching response.');
+            }
+            return response.json();
+        })
+        .then(data => {
+            if (data === false) {
+                throw new Error('Error fetching data.');
+            }
+
+            setFirstName(data.data[0].firstname);
+            setLastName(data.data[0].lastname);
+            setEmail(data.data[0].email);
+            setBirthday(data.data[0].birthday);
+            setGender(data.data[0].gender);
+            setAddress(data.data[0].address.street);
+            setCity(data.data[0].address.city);
+            setCountry(data.data[0].address.country);
+
+            setShowPerson(true);
+        });
     }
 
     return (
@@ -59,15 +68,34 @@ function Hw3() {
             <h1 className="fade-in" style={{marginTop: "5rem"}}>Homework 3<br/>Faker</h1>
             <p className="fade-in delay-1s">Fetch any fake data at the click of a button.</p>
 
-            <div className="content">
-                <button
+            <div style = {{marginTop: "1rem"}}>
+                <button className="btn-hw3"
                     onClick={handlePerson}
                     > Fake Person
                 </button>
 
-                <p onChange={handlePerson}>
-                    Name: {firstName} {lastName} 
-                </p>
+                <button className="btn-hw3"
+                    onClick={handlePerson}
+                    > Fake Company
+                </button>
+
+                <div style= {{marginTop: '2rem'}}>
+                    <p style ={{ display: showPerson ? 'block' : 'none'}}>
+                        Name: {firstName} {lastName}
+                        <br></br>
+                        Email: {email}
+                        <br></br>
+                        Birthday: {birthday}
+                        <br></br>
+                        <span style={{ textTransform: 'capitalize' }}>Gender: {gender}</span>
+                        <br></br>
+                        Address: {address}, {city}
+                        <br></br>
+                        {country}
+                    </p>
+
+                </div>
+                
             </div>
 
 
